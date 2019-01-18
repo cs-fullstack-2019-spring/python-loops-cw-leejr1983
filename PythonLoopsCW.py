4c4ab2b488f71db8c4265fa262a5914fdf917b6a
leejr1983 <leejr1983@yahoo.com>
2019-01-18 15:50:11 -0600

    classwork

---
+++ PythonLoopsCW.py
@@ -0,0 +1,29 @@
+def main():
+
+    problem1()
+
+#     Exercise 1:
+#
+# Print -20 to and including 50.
+# Use any loop you want.
+
+def    problem1():
+
+        print("I'm wasting your and my time, as you can see I couldn't get past problem 1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
